0715eca1c59259d567f5911a46863c8fc61ebbd3
shiiva11 <124708326+shiiva11@users.noreply.github.com>
2023-02-12 03:55:27 +0530

    Update and rename participate.html to participate.php

--- participate.php
+++ participate.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+
+if(isset($_SESSION['username']))
+  {
+    echo $_SESSION['username'];
+    ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -142,3 +150,16 @@
 
 </body>
 </html>
+
+
+
+<?php
+  }
+
+   else{
+    echo " please login to continue.";
+     header ("Location: login.html");
+     }
+?>
+
+
